03c12f4f6db04894ec9e2f44417f39653abc826d
Zachary Bray <zachbray9@knights.ucf.edu>
2025-10-23 01:28:01 -0400

    Fixed issue where create anime form wasn't working for select options
    Fixed issue where create anime form was only using the first letter of the selected option due to changes in the FormSelect component.

--- react-client/src/schemas/createAnimeSchema.ts
+++ react-client/src/schemas/createAnimeSchema.ts
@@ -3,18 +3,18 @@ import z from "zod";
 export const createAnimeValidationSchema = z.object({
     englishTitle: z.string().min(1, "English title is required"),
     romajiTitle: z.string().min(1, "Romaji title is required"),
-    synopsis: z.string().min(1, "Synopsis is required."),
-    format: z.array(z.string()).min(1, "Format is required"),
+    synopsis: z.string().min(1, "Synopsis is required"),
+    format: z.string().min(1, "Format is required"),
     genres: z.array(z.string()).min(1, "Genre(s) is required"),
     episodes: z.number().min(0, "Episodes must be a positive number"),
     duration: z.number().positive("Duration must be a positive number"),
-    studio: z.array(z.string()).min(1, "Studio is required"),
+    studio: z.string().min(1, "Studio is required"),
     startDate: z.string(),
     endDate: z.string(),
-    season: z.array(z.string()).min(1, "Season is required"),
+    season: z.string().min(1, "season is required"),
     seasonYear: z.number().min(1900, "Anime wasn't even invented until 1917 bozo").max(2100, "Invalid year"),
     trailerUrl: z.string().optional().or(z.literal("")),
-    ageRating: z.array(z.string()).min(1, "Age rating is required"),
+    ageRating: z.string().min(1, "Age rating is required"),
     isAdult: z.boolean(),
     poster: z.instanceof(File, { error: "Poster image is required" })
         .refine((file) => file.size <= 5000000, "File must be less than 5MB")
