ddc2265db35f824c47e4dc207e43cbc6b098ea1e
Zachary Bray <zachbray9@knights.ucf.edu>
2025-10-30 01:34:20 -0400

    Removed unused aniListApi agent and queries.
    Removed aniListAgent and queries since the app doesn't call the AniList api anymore.

--- react-client/src/api/axios.ts
+++ react-client/src/api/axios.ts
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-export const aniListApi = axios.create({
-    baseURL: 'https://graphql.anilist.co'
-})
-
 export const myApi = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
